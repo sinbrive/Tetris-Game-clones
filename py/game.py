eef6a87ef148e0baee543a63f1f92dc4818171ca
Eurientis <sinbrive@users.noreply.github.com>
2022-12-09 19:27:21 +0100

    some refactoring

--- py/game.py
+++ py/game.py
@@ -28,29 +28,28 @@ class Game:
       self.lockCurrentShape()  
       self.switchToNextShape()
       self.checkUpdateScore()
+    self.checkGameOver()
       
     
-
   def draw(self):
     self.displayGrid()
     self.shape.draw()
     self.drawLines()
     self.displaySideBoard()
-    if (self.game_over):
-        fill(255,100,100)
-        text("Game Over", width-70, height/2+150)
-
+    self.displayGameOver()
   
   def makeStepDown(self): 
     if (millis() - self.chrono > self.timeLevel):
       self.chrono = millis()
       self.shape.move(0, 20)
-    
 
   def nextOnFloor(self):
     return not (self.shape.highestY() < height)
   
-  
+  def lockCurrentShape(self):  
+    arr = self.shape.getRealCoords()
+    self.lines+=arr
+    
   def switchToNextShape(self):
     self.rotIndex = 0
     self.chrono = millis()
@@ -58,17 +57,31 @@ class Game:
     self.shape.setXoffset(10)
     self.shape.setYoffset(10)
     self.nextShape = Shape(width-70, 50)
-  
-
-  def displaySideBoard(self):
-    fill('#222222')
-    rect(width-50, height/2, 100, height)
-    self.nextShape.draw()
-    fill(130)
-    textSize(15)
-    text("Level "+str(self.level), width-80, height/2)
-    text("Score "+str(self.score), width-80, height/2+50)
-  
+      
+  def checkUpdateScore(self):
+    for i in range(len(self.lines)):
+      row = height - i * 20 - 10
+      count=len(list(filter((lambda item : item.y == row), self.lines)))
+      if count==0: break
+      if count == 10:
+        self.removeLine(row)
+        self.score+=20
+        if self.score > self.points_per_level*self.level:
+          self.score=0
+          self.level+=1
+        return
+ 
+  def removeLine(self,row):
+    self.lines = list(filter(lambda item: item.y != row, self.lines))
+    for i in range(len(self.lines)):
+      ln = self.lines[i]
+      if ln.y < row:
+        ln.y += 20
+        self.lines[i].y = ln.y
+        
+  def checkGameOver(self):
+    res = len(list(filter((lambda item : item.y < 60), self.lines)))
+    self.game_over= res!=0
 
   def displayGrid(self):
     noFill()
@@ -77,51 +90,30 @@ class Game:
     for y in range(10, height, 20):
       for x in range(10, width-100, 20):
         rect(x, y, 20, 20)
-
-  def lockCurrentShape(self):  
-    arr = self.shape.getRealCoords()
-    for i in range(len(arr)):
-      self.lines.append(arr[i])
-    
-  def checkUpdateScore(self):
-    for i in range(len(self.lines)):
-      row = height - i * 20 - 10
-      count=len(list(filter((lambda item : item.y == row), self.lines)))
-      if count == 10:
-        self.removeLine(row)
-        self.score+=20
-        if self.score > self.points_per_level*self.level:
-          self.score=0
-          self.level+=1
-        return
-    for item in self.lines:
-      if item.y < 60:
-        self.game_over = True
-        return
-
-  def removeLine(self,row):
-    self.lines = list(filter(lambda item: item.y != row, self.lines))
-    for i in range(len(self.lines)):
-      ln = self.lines[i]
-      if ln.y < row:
-        ln.y += 20
-        self.lines[i].y = ln.y
-    
-  
-
+            
   def drawLines(self):
     for i in range(len(self.lines)):
       ln = self.lines[i]
       fill(ln.c)
       rect(ln.x, ln.y, 20, 20)
     
+  def displaySideBoard(self):
+    fill('#222222')
+    rect(width-50, height/2, 100, height)
+    self.nextShape.draw()
+    fill(130)
+    textSize(15)
+    text("Level {}".format(self.level), width-80, height/2)
+    text("Score {}".format(self.score), width-80, height/2+50)
   
-
+  def displayGameOver(self):
+    if (self.game_over):
+        fill(255,100,100)
+        text("Game Over", width-80, height/2+150)
+  
   def inputKey(self,key):
-
     x=0
     y=0
-
     if (key == RIGHT):
       x = self.shape.getXoffset()
       if (x < width-100 - 20 * self.shape.getNbX()): self.shape.move(20,0)
